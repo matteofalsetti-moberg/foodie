31d45d0dabfbb9834e345622a651121ac6c5cad4
matteofalsetti-moberg <matteo.falsetti@moberg.hr>
2022-11-29 16:19:33 +0100

    minor css changes

--- src/Containers/Modal/ModalCart.tsx
+++ src/Containers/Modal/ModalCart.tsx
@@ -1,20 +1,25 @@
 import ModalHeader from "../../components/Modal/modalHeader";
 import ModalBody from "../../components/Modal/modalBody";
 import ModalFooter from "../../components/Modal/modalFooter";
-import "./ModalCart.scss"
+import "./ModalCart.scss";
+import Card from "../../components/Card/Card";
 
-interface IModalCart{
-    onCloseModal: any,
+interface IModalCart {
+    onCloseModal: any;
 }
 
-function ModalCart({onCloseModal}: IModalCart){
-    return (<div className="modal" onClick={onCloseModal} >
-        <div className="modal__content" >
-    <ModalHeader />
-    <ModalBody />
-    <ModalFooter onCloseModal={onCloseModal} />
-    </div>
-    </div>)
+function ModalCart({ onCloseModal }: IModalCart) {
+    return (
+        <div className="modal">
+            <Card>
+                <div className="modal__content">
+                    <ModalHeader />
+                    <ModalBody />
+                    <ModalFooter onCloseModal={onCloseModal} />
+                </div>
+            </Card>
+        </div>
+    );
 }
 
 export default ModalCart;
