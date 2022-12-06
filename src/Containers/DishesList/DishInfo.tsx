edfc7daa37ded8fd3e7272f19b3a861bcf855d75
matteofalsetti-moberg <matteo.falsetti@moberg.hr>
2022-12-06 16:49:05 +0100

    Dish info implemented
    nedd to add scss and more data about the dish

--- src/Containers/DishesList/DishInfo.tsx
+++ src/Containers/DishesList/DishInfo.tsx
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Item from "../../components/Item/Item";
 
-export class DishInfo extends Component {
-  render() {
-    return (
-      <div>DishInfo</div>
-    )
-  }
+function DishInfo() {
+    const { id } = useParams();
+    const [dish, setDish] = useState({ strMealThumb: "", strMeal: "" });
+
+    useEffect(() => {
+        async function fetchDishInfo() {
+            const response = await fetch(
+                `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+            );
+            const dish = await response.json();
+            setDish(dish.meals[0]);
+            console.log(dish);
+        }
+        fetchDishInfo();
+    }, []);
+
+    return  <Item image={dish.strMealThumb} name={dish.strMeal} />;
 }
 
-export default DishInfo
+export default DishInfo;
