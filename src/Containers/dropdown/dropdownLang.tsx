5b3d813c0b2e4c71085051162ecd0f4b78778a81
matteofalsetti-moberg <matteo.falsetti@moberg.hr>
2022-12-07 14:19:48 +0100

    added flags

--- src/Containers/dropdown/dropdownLang.tsx
+++ src/Containers/dropdown/dropdownLang.tsx
@@ -2,28 +2,31 @@ import Dropdown from "react-bootstrap/Dropdown";
 import { DropdownButton } from "react-bootstrap";
 import "./dropdownMenu.scss";
 import i18next from "../../Translation";
-
-
-
+import { useTranslation } from "react-i18next";
+import eng from "../../assets/englandFlag.png";
+import cro from "../../assets/croatianFlag.png";
 
 function DropdownLang() {
-  function changeLang(lang: "en" | "hr") {
-    i18next.changeLanguage(lang);
-}
+    const { t } = useTranslation();
+    function changeLang(lang: "en" | "hr") {
+        i18next.changeLanguage(lang);
+    }
 
     return (
-        <DropdownButton variant="dropMenu" title="Language">
+        <DropdownButton variant="dropMenu" title={t("Lang")}>
             <Dropdown.Item
                 className="dropMenu__link"
                 onClick={() => changeLang("en")}
             >
                 English
+                <img className="dropMenu__img" src={eng} />
             </Dropdown.Item>
             <Dropdown.Item
                 className="dropMenu__link"
                 onClick={() => changeLang("hr")}
             >
-                Croatian
+                Hrvatski
+                <img className="dropMenu__img" src={cro} />
             </Dropdown.Item>
         </DropdownButton>
     );
